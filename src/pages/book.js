b3a8f071e44f13db40741a314f3b72cb89797134
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-21 10:59:53 -0300

    Setando varáveis no estado

--- src/pages/book.js
+++ src/pages/book.js
@@ -1,6 +1,52 @@
-import React from 'react' 
-import {View} from 'react-native'
+import React, {useState} from 'react'
+import { Text,TextInput, SafeAreaView,StyleSheet, AsyncStorage } from 'react-native'
 
-export default function Book(){
-    return <View />
+export default function Book({ navigation }) {
+    const [date, setDate]= ('');
+    const id = navigation.getParam('id');
+    return (
+        <SafeAreaView>
+            <Text style={styles.laber}>DATA DE INTERESSE *</Text>
+            <TextInput 
+            style={styles.input}
+            placeholder="Qualdata você quer reservar"
+            placeholderTextColor="#999"
+            autoCapitalize= "words"
+            autoCorrect={false}
+            value={date}
+            onChangeText={setDate}
+            />
+        </SafeAreaView>
+        )
+
 }
+
+const styles = StyleSheet.create({
+    label: {
+        fontWeight: 'bold', 
+        color: '#444', 
+        marginBottom: 8
+    },
+    input: {
+        borderWidth: 1, 
+        borderColor: "#ddd", 
+        paddingHorizontal: 20,
+        fontSize: 16, 
+        color: "#444", 
+        height: 44, 
+        marginBottom: 20, 
+        borderRadius: 2
+    }, 
+    button: {
+        height: 42, 
+        backgroundColor: '#f05a5b', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        borderRadius: 2
+    }, 
+    buttonText: {
+        color: '#fff', 
+        fontWeight: "bold", 
+        fontSize: 16,
+    }
+})
